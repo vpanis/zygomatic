c04fd8cba89ad456521d2fe52bb7bf092a3fd592
Benjamin Poucin <bpoucin@gmail.com>
2017-05-31 20:42:01 +0200

    ok til the review

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,49 @@ require 'csv'
 csv_comedians = File.read(Rails.root.join('lib', 'seeds', 'comedians.csv'))
 csv = CSV.parse(csv_comedians, :headers => true, col_sep: ';')
 puts csv_comedians
-
 csv.each do |row|
   comedian = Comedian.new(name: row[0], short_description: row[2], full_description: row[3])
   comedian.picture_url = row[1]
   p comedian
   comedian.save!
-
 end
 
-# csv_skits = File.read(Rails.root.join('lib', 'seeds', 'skits.csv'))
-# csv = CSV.parse(csv_skits, :headers => true, col_sep: ';')
-# puts csv_skits
+csv_skits = File.read(Rails.root.join('lib', 'seeds', 'skits.csv'))
+csv = CSV.parse(csv_skits, :headers => true, col_sep: ';')
+puts csv_skits
+csv.each do |row|
+  Skit.create(name: row[0], comedian_id: row[1], youtube_path: row[2], duration: row[3], nb_of_views: row[4], short_description: row[5], full_description: row[6], category: row[7])
+end
 
-# csv.each do |skit|
-#   Skit.create(name: skit[0], comedian_id: skit[1], youtube_path: skit[2], duration: skit[3], nb_of_views: skit    dian[1], full_description: comedian[2])
-# end
+csv_users = File.read(Rails.root.join('lib', 'seeds', 'users.csv'))
+csv = CSV.parse(csv_users, :headers => true, col_sep: ';')
+puts csv_users
+csv.each do |row|
+  user = User.create(first_name: row[0], last_name: row[1], email: row[2], fun_level: row[3], password: row[4])
+  user.picture_url = row[4]
+  p user.save!
+end
+
+csv_reviews = File.read(Rails.root.join('lib', 'seeds', 'reviews.csv'))
+csv = CSV.parse(csv_reviews, :headers => true, col_sep: ';')
+puts csv_reviews
+csv.each do |row|
+  Review.create(content: row[0], rating: row[1], user: row[2], skit_id: row[3])
+end
+
+csv_playlists = File.read(Rails.root.join('lib', 'seeds', 'playlists.csv'))
+csv = CSV.parse(csv_playlists, :headers => true, col_sep: ';')
+puts csv_playlists
+csv.each do |row|
+  Playlist.create(name: row[0], user: row[1])
+end
+
+csv_playlists_skits = File.read(Rails.root.join('lib', 'seeds', 'csv_playlists_skits.csv'))
+csv = CSV.parse(csv_playlists_skits, :headers => true, col_sep: ';')
+puts csv_playlists_skits
+csv.each do |row|
+  Playlistskit.create(skit_id: row[0], playlist_id: row[1])
+end
+
+
+
