b8b22d1ac3adff4d8db9f28b368faaca9f6bd597
Xavier Martinet <xavier.martinet@gmail.com>
2017-06-01 13:10:26 +0200

    carousel implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @recommended_skits = find_recommended_skits(6)
+  end
+
+  private
+
+  def find_recommended_skits(nb_of_skits)
+    RecommendedSkitsService.find(skit: @skit, user: current_user, nb_of_skits: nb_of_skits)
   end
 end
