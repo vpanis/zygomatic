0746a24020aba2f9d6b98e06abe82e440936dd83
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-06-02 15:11:57 +0200

    Ajaxification working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,8 @@ class ReviewsController < ApplicationController
       nth_rating = @review.rating
       nb_of_reviews = @skit.reviews.count
       @skit.average_rating = (nth_rating + (@skit.average_rating * (nb_of_reviews - 1))).fdiv(nb_of_reviews)
+      @skit_rating_average = rating_average(@skit)
+
       @skit.save!
       respond_to do |format|
         format.html { redirect_to skit_path(@review.skit), notice: "Yeah ! Thanks for this rating !" }
@@ -19,6 +21,14 @@ class ReviewsController < ApplicationController
         format.js  # <-- will render `app/views/reviews/create.js.erb`
       end
     end
+  end
+
+  def rating_average(skit)
+    ratings = []
+    skit.reviews.each do |review|
+      ratings << review.rating
+    end
+    return ratings.inject{ |sum, rate| sum + rate }.to_f / ratings.length
   end
 
   def review_params
