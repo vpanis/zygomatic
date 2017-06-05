100d4e563dd1a557e13e9b04a394e5b7281bfad0
Xavier Martinet <xavier.martinet@gmail.com>
2017-06-05 11:48:44 +0200

    meta tags facebook and twitter implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :store_current_location, :unless => :devise_controller?
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   private
     # override the devise helper to store the current location so we can
     # redirect to it after loggin in or out. This override makes signing in
