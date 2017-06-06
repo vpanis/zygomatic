a2b3a124760c4c99ed534d35934bf41074790256
Philippe Coste <philippecostelh@gmail.com>
2017-06-06 17:49:17 +0200

    Il ne reste plus qu'à centrer et qu'à connecter avec la pile

--- app/assets/javascripts/quizz.js
+++ app/assets/javascripts/quizz.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-  $(".quizzbox").click(function(){
-      if ($(this).next(".quizzbox").length > 0) {
-        $(this).hide();
-        $(this).next(".quizzbox").show();
+  $(".next-card").click(function(){
+  	var current_quizz = $(this).parents(".quizzbox:first");
+  	var next_quizz = $(current_quizz).next(".quizzbox");
+      if (next_quizz.length > 0) {
+      	// $(current_quizz).addClass('bravo');
+        $(current_quizz).hide(500);
+        $(next_quizz).show(500);
       }
     });
+});
+
+$(document).ready(function(){ 
+  $(".bad-answer").click(function(){
+        $(this).parents(".quizzbox:first").effect("shake");
+				e.stopPropagation();
+   });
 });
